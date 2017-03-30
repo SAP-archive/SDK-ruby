fc7ea31bd97b5af4d9bb95a1e9491fb2f359b09e
Paul Renvoisé <renvoisepaul@gmail.com>
2017-03-30 10:57:53 +0200

    [ADD] voice file converse

--- lib/recastai/apis/request/converse.rb
+++ lib/recastai/apis/request/converse.rb
@@ -26,5 +26,25 @@ module RecastAI
 
       Conversation.new(response.body, token)
     end
+
+    def analyse_file(file, token: nil, language: nil, conversation_token: nil, memory: nil)
+      token ||= @token
+      raise RecastError.new('Token is missing') if token.nil?
+
+      language ||= @language
+
+      body = { voice: File.new(file) }
+      body[:language] = language unless language.nil?
+      body[:conversation_token] = conversation_token unless conversation_token.nil?
+      body[:memory] = memory unless memory.nil?
+      response = HTTMultiParty.post(
+        Utils::CONVERSE_ENDPOINT,
+        body: body,
+        headers: { 'Authorization' => "Token #{token}" }
+      )
+      raise RecastError.new(JSON.parse(response.body)['message']) if response.code != 200
+
+      Conversation.new(response.body, token)
+    end
   end
 end
