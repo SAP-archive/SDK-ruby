0f861ee7e5e65df9cfc2b918a673dbc256b66a22
Doriane <doriane.quartino@sap.com>
2018-07-31 14:31:34 +0200

    Audio endpoint deprecated

--- lib/recastai/apis/request/analyse.rb
+++ lib/recastai/apis/request/analyse.rb
@@ -23,23 +23,5 @@ module RecastAI
 
       Response.new(JSON.parse(response.body))
     end
-
-    def analyse_file(file, token: nil, language: nil)
-      token ||= @token
-      raise RecastError.new('Token is missing') if token.nil?
-
-      language ||= @language
-
-      body = { voice: File.new(file) }
-      body[:language] = language unless language.nil?
-      response = HTTMultiParty.post(
-        Utils::REQUEST_ENDPOINT,
-        body: body,
-        headers: { 'Authorization' => "Token #{token}" }
-      )
-      raise RecastError.new(JSON.parse(response.body)['message']) if response.code != 200
-
-      Response.new(JSON.parse(response.body))
-    end
   end
 end
